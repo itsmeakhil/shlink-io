5438a9c81ebd2055192faaa876e2e98025e7ddab
itsmeakhil <akhiledathadan007@gmail.com>
2025-01-21 18:10:43 +0530

    ui enhancements

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 from app.api import api as api_router
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (update for production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from FastAPI!"}
-
+    # Load the HTML file
+    html_file = Path("templates/index.html").read_text()
+    return HTMLResponse(content=html_file)
 
 app.include_router(api_router.router)
 
